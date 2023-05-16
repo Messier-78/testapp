0a1a34380fe28978e2e1d0e6bb8a270043b1b1be
Messier-78 <bsg0909@gmail.com>
2023-05-16 17:37:58 +0900

    correction static

--- testapp/settings.py
+++ testapp/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 import os
 
 STATIC_URL = 'static/'
-STATIC.ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
